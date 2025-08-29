552f44c8f43f52ce73aa22263f1f5617c7747830
Loretta Chapman <93103573+Dessiidoo@users.noreply.github.com>
2025-08-29 19:41:08 -0400

    Clear project and start fresh

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello World!</h1>
+    <p>Your new project is ready. What would you like to build?</p>
+  </div>
+`
